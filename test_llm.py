439aad7faf7c14ca6aa9925e4d736b9db82a85a7
Karthik-005 <pvkarthik173@gmail.com>
2025-07-18 12:23:03 +0530

    Added the API call file to the repo

---
+++ test_llm.py
@@ -0,0 +1,16 @@
+from llama_cpp import Llama
+
+
+model_path = "/home/karthik/.cache/huggingface/hub/models--TheBloke--CapybaraHermes-2.5-Mistral-7B-GGUF/snapshots/8bea614edd9a2d5d9985a6e6c1ecc166261cacb8/capybarahermes-2.5-mistral-7b.Q2_K.gguf"
+
+
+model = Llama(model_path=model_path, n_ctx=128, n_threads=4,n_batch=32,use_mlock=False,verbose=False)
+
+
+prompt = input("Enter your PR : ")
+output = model(prompt=prompt,max_tokens=256,temperature=0.5,top_k=50,top_p=0.9,stop=["</s>"])
+
+
+print("Output:")
+print(output["choices"][0]["text"].strip())
+
