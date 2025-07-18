12f56386f9ca23ef0f63c335c8864c0a96127577
Karthik-005 <pvkarthik173@gmail.com>
2025-07-18 12:31:41 +0530

    Integrated the API to the webhook file

--- test_llm.py
+++ test_llm.py
@@ -1,16 +1,26 @@
 from llama_cpp import Llama
 
 
-model_path = "/home/karthik/.cache/huggingface/hub/models--TheBloke--CapybaraHermes-2.5-Mistral-7B-GGUF/snapshots/8bea614edd9a2d5d9985a6e6c1ecc166261cacb8/capybarahermes-2.5-mistral-7b.Q2_K.gguf"
+model_path = "/home/karthik/.cache/huggingface/hub/models--TheBloke--CapybaraHermes-2.5-Mistral-7B-GGUF/snapshots/	8bea614edd9a2d5d9985a6e6c1ecc166261cacb8/capybarahermes-2.5-mistral-7b.Q2_K.gguf"
+
+def get_commit(PR) :
+	model = Llama(model_path=model_path, n_ctx=128, n_threads=4,n_batch=32,use_mlock=False,verbose=False)
 
 
-model = Llama(model_path=model_path, n_ctx=128, n_threads=4,n_batch=32,use_mlock=False,verbose=False)
+	prompt = f"""You are an expert software engineer. Based on the following GitHub pull request details, generate a short, clear, and meaningful commit message that summarizes the changes.
+
+Pull Request Title:
+{pr['title']}
+
+Pull Request Description:
+{pr['body']}
+
+Respond with only the commit message, no extra text.
+"""
+
+	output = model(prompt=prompt,max_tokens=256,temperature=0.5,top_k=50,top_p=0.9,stop=["</s>"])
 
 
-prompt = input("Enter your PR : ")
-output = model(prompt=prompt,max_tokens=256,temperature=0.5,top_k=50,top_p=0.9,stop=["</s>"])
 
-
-print("Output:")
-print(output["choices"][0]["text"].strip())
+	print(output["choices"][0]["text"].strip())
 
